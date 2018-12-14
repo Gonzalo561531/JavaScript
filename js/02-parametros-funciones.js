853ea592c8964c30998c5f4136f558f1a1c1d2c1
Gonzalo561531 <miki.gonzalo56@gmail.com>
2018-12-14 11:57:45 -0500

    funciones-parametros

---
+++ js/02-parametros-funciones.js
@@ -0,0 +1,12 @@
+'use strict'
+//FUNCIONES: Una funcion es una agrupacion de istrucciones reutilizables
+//Defino la funcion
+function calculadora(numero1,numero2){
+    //conjuntos de istrucciones a ejecutar
+    console.log("Suma: "+(numero1+numero2));
+    console.log("Resta: "+(numero1-numero2));
+    console.log("Multiplicacion: "+(numero1*numero2));
+    console.log("Divicion: "+(numero1/numero2));
+}
+//Ivocar y llamar a la función
+calculadora(12,8);
