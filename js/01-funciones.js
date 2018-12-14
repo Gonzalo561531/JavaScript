25c41c155e1c3d4bde51afbdcaa395820c50231e
Gonzalo561531 <miki.gonzalo56@gmail.com>
2018-12-14 11:47:56 -0500

    funciones

---
+++ js/01-funciones.js
@@ -0,0 +1,11 @@
+'use strict'
+//FUNCIONES: Una funcion es una agrupacion de istrucciones reutilizables
+//Defino la funcion
+function calculadora(){
+	console.log("hola soy la calculadora!!");
+	// return "hola soy la calculadora".. devuelve algo a la funcion
+}
+//Ivocar y llamar a la función
+calculadora();
+
+
